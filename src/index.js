7c6539e48fbb08137b1b3b37f39823a250a93185
Oleg Chernyavskyi <ochernyavskyi@gmail.com>
2021-02-12 17:19:54 +0200

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const store = new Map();
 
-const fib = (value) => {
+function* fib  (value) {
     if (store.has(value)) {
         return store.get(value);
     }
     let result = 0;
     for (let i = 1; i <= value; i++) {
         result += i;
-        store.set(i, result);
+        yield store.set(i, result);
     }
     store.set(value, result);
     return result;
 
 }
+
+let a = fib(3);
+console.log (a.next());
+console.log (a.next());
+console.log (a.next());
+console.log (a.next());
